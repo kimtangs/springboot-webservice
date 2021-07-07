c848a5bb9f6f7b8db944e2f9d8a5261fc7deb61c
TY <kimtangs@gmail.com>
2021-07-08 00:27:09 +0900

    배포 자동화 구성#3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,7 +21,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
 #CURRENT_PID=$(pgrep -fl springboot-webservice | grep jar | awk '{print $1}')
-CURRENT_PID=$(pgrep -fl springboot-1.0-SNAPSHOT | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot | grep java | awk '{print $1}')
 
 echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
